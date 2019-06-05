5a26ce0b2492ed732305db50024dce2b59e34122
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-05 15:12:11 -0300

    controllers and routes integration

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,16 +1,9 @@
 import express from 'express'
+import PostsController from '../controllers/posts'
 
 const router = express.Router()
+const postsController = new PostsController()
 
-router.get('/', (req, res) =>
-  res.send([
-    {
-      title: 'Default title',
-      description: 'Default description',
-      author: 'Default author',
-      likes: 100
-    }
-  ])
-)
+router.get('/', (req, res) => postsController.getAll(req, res))
 
 export default router
