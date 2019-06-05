41c82bc9113386b266ef1df4619182214888b8f7
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-05 15:51:49 -0300

    config mongodb to API connect

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-import app from './src/app'
+import setupApp from './src/app'
+
 const port = 3000
 
-app.listen(port, () => {
-  console.table([['port', `${port}`]])
-})
+setupApp()
+  .then(app =>
+    app.listen(port, () => {
+      console.table([['port', `${port}`]])
+    })
+  )
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import routes from './routes'
+import database from './config/database'
 
 const app = express()
-app.use(bodyParser.json())
 
-app.use('/api', routes)
+const configureExpress = () => {
+  app.use(bodyParser.json())
+  app.use('/', routes)
 
-export default app
+  return app
+}
+
+export default () => database.connect().then(configureExpress)
