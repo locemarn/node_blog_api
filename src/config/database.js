144590cb6a92b674645384c70bb9b679fd3e8461
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-06 14:15:45 -0300

    refactoring integration test

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,10 @@ mongoose.Promise = Promise
 
 const mongodbUrl = process.env.DB_MONGODB_URL || process.env.DB_MONGODB_URL_TEST
 
-const connect = () => mongoose.connect(mongodbUrl, { useNewUrlParser: true })
+const connect = () =>
+  mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true
+  })
 
 export default {
   connect
